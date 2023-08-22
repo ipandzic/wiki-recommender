59bac990cf9e558ec869885671f4c09962f73b07
ipandzic <ivan.pandzic@gmail.com>
2023-08-22 22:33:03 +0200

    Update get_recommended_links

--- recommender/core/views.py
+++ recommender/core/views.py
@@ -1,53 +1,41 @@
-from urllib.parse import urlparse
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
 from django.http import JsonResponse
-from scrapy.utils.project import get_project_settings
-from scrapy.crawler import CrawlerRunner
-from twisted.internet import defer
-from channels.db import database_sync_to_async
-import scrapy
 
 
-class WikipediaSpider(scrapy.Spider):
-    name = 'wikipedia'
+def get_links_with_beautiful_soup(url):
+    response = requests.get(url)
+    if response.status_code != 200:
+        return []
 
-    def parse(self, response):
-        parsed_uri = urlparse(self.start_urls[0])
-        self.base_url = '{uri.scheme}://{uri.netloc}'.format(uri=parsed_uri)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    base_url = url
 
-        recommended_links = []
+    recommended_links = []
 
-        if response.status == 200:
-            for link in response.xpath('//div[@class = "vector-body"]//a'):
-                candidate_link = link.xpath('.//@href').get()
-                if candidate_link is not None:
-                    candidate_link = candidate_link.split('#')[0]
-                    if candidate_link != '' and candidate_link[:4] != 'http' and candidate_link[:2] != '//':
-                        recommended_links.append(self.base_url + candidate_link)
-            for link in response.xpath('//a[@class = "external text"]'):
-                candidate_link = link.xpath('.//@href').get()
-                if candidate_link != '' and candidate_link[:2] != '//':
-                    recommended_links.append(candidate_link)
+    # Extract links from the main content area
+    for link in soup.select('div.vector-body a'):
+        href = link.get('href')
+        if href:
+            full_url = urljoin(base_url, href.split('#')[0])
+            recommended_links.append(full_url)
 
-        return recommended_links
+    # Extract external links
+    for link in soup.select('a.external.text'):
+        href = link.get('href')
+        if href and href[:2] != '//':
+            recommended_links.append(href)
 
-
-@database_sync_to_async
-def run_spider(url):
-    runner = CrawlerRunner(get_project_settings())
-    deferred = runner.crawl(WikipediaSpider, start_urls=[url])
-    result = yield deferred  # Use yield to extract the result from the Deferred
-    return result
+    return recommended_links
 
 
-async def get_recommended_links(request):
+def get_recommended_links(request):
     url = request.GET.get('url')
     if not url:
         return JsonResponse({"error": "URL parameter is missing."}, status=400)
 
-    links = await run_spider(url)
-
-    # Ensure the result is a list before sending it in the JsonResponse
-    if not isinstance(links, list):
-        links = []
-
+    links = get_links_with_beautiful_soup(url)
     return JsonResponse({"recommended_links": links})
+
