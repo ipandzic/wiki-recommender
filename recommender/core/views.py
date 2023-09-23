4cb3ced8812da78e9d2473bbd8fe9098f0d06fb7
ipandzic <ivan.pandzic@gmail.com>
2023-09-23 19:39:57 +0200

    Update get_links_with_beautiful_soup

--- recommender/core/views.py
+++ recommender/core/views.py
@@ -1,16 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
-
+from collections import Counter
 from django.http import JsonResponse
 
 from core.models import CandidatePages, CrawledPages
 
 
-def get_links_with_beautiful_soup(url):
-    # Check if the URL has been already crawled
-    if CrawledPages.objects.filter(page=url).exists():
-        return []
+def get_links_with_beautiful_soup(url, max_links=20):
+    # Initialize empty list to hold the crawled pages
+    crawled_pages = [obj.page for obj in CrawledPages.objects.all()]
 
     # Fetch and parse the web page
     response = requests.get(url)
@@ -18,9 +17,8 @@ def get_links_with_beautiful_soup(url):
         return []
 
     soup = BeautifulSoup(response.content, 'html.parser')
-    base_url = url
 
-    recommended_links = []
+    all_links = []
 
     # Extract links from the main content area
     for link in soup.select('div.vector-body a'):
@@ -30,31 +28,33 @@ def get_links_with_beautiful_soup(url):
 
         href = link.get('href')
         if href:
-            full_url = urljoin(base_url, href.split('#')[0])
-            recommended_links.append(full_url)
+            full_url = urljoin(url, href.split('#')[0])
 
-            # Add to CandidatePages if not already there
+            # Apply multiple exclusion conditions
+            if (full_url in crawled_pages or
+                    full_url.startswith("https://en.wikipedia.org/wiki/Wikipedia") or
+                    "(disambiguation)" in full_url or
+                    full_url.endswith('.png') or
+                    "(identifier)" in full_url or
+                    full_url in ["https://en.wikipedia.org/wiki/Surname", "https://en.wikipedia.org/wiki/Given_name"]):
+                continue
+
+            all_links.append(full_url)
+
             if not CandidatePages.objects.filter(page=full_url).exists():
-                CandidatePages.objects.create(page=full_url, rate=0)  # Set initial rate to 0
+                CandidatePages.objects.create(page=full_url, rate=0)
 
-    # Extract external links
-    for link in soup.select('a.external.text'):
-        parent = link.find_parent(['div', 'span'], {'class': 'reflist', 'id': ['References', 'Citations']})
-        if parent:
-            continue  # Skip links under "References" section
+    # Count the frequency of each link
+    link_count = Counter(all_links)
 
-        href = link.get('href')
-        if href and href[:2] != '//':
-            recommended_links.append(href)
+    # Get the most common links based on their frequency
+    most_common_links = [item[0] for item in link_count.most_common(max_links)]
 
-            # Add to CandidatePages if not already there
-            if not CandidatePages.objects.filter(page=href).exists():
-                CandidatePages.objects.create(page=href, rate=0)  # Set initial rate to 0
+    # Add the URL to the CrawledPages table, making it eligible for future crawling
+    if not CrawledPages.objects.filter(page=url).exists():
+        CrawledPages.objects.create(page=url)
 
-    # Mark the URL as crawled
-    CrawledPages.objects.create(page=url)
-
-    return recommended_links
+    return most_common_links
 
 
 def get_recommended_links(request):
